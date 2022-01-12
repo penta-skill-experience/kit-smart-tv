cfa929014f98ddf80a6270c51f091cfda5537890
tom-mohr <tom-mohr@users.noreply.github.com>
2022-01-12 15:14:41 +0100

    removed babel and astroturf from webpack

--- client.webpack.config.js
+++ client.webpack.config.js
@@ -13,26 +13,6 @@ module.exports = env => ({
                 use: ["style-loader", "css-loader", "postcss-loader"],
             },
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: [
-                                "@babel/preset-env",
-                                "@babel/preset-react",
-                                "@babel/preset-typescript"
-                            ],
-                            plugins: [
-                                "@babel/plugin-proposal-class-properties"
-                            ],
-                        },
-                    },
-                    "astroturf/loader"
-                ],
-            },
-            {
                 test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
                 use: ["ts-loader"],
