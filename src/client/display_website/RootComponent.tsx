2723119585ee7c98740f7f0973eab9be4987a3ec
Morris BE <ubsmd@student.kit.edu>
2022-01-15 21:08:15 +0100

    Improved the grid layout by adding transparency, blur and shadows, added a background image and a Technologiefabrik logo.

--- src/client/display_website/RootComponent.tsx
+++ src/client/display_website/RootComponent.tsx
@@ -1,33 +1,29 @@
 import * as React from "react";
 import {SquareHolder} from "./SquareHolder";
-
 export class RootComponent extends React.Component<any, any> {
     render() {
-        return <section className="h-screen bg-[#F3F4F6]">
-            <div className="flex h-screen justify-center">
-                <div className="grid place-items-center">
-                <div className="grid gap-x-60 gap-y-30 lg:grid-cols-3 xl:grid-cols-3">
-                    <div className="w-full">
-                        <SquareHolder />
+        return <div className = "w-screen h-screen bg-no-repeat bg-cover bg-center" style={{
+            backgroundImage: `url("https://images.wallpaperscraft.com/image/single/city_skyscrapers_clouds_rain_road_cars_lights_58563_3840x2160.jpg")`
+        }}>
+            <div className="w-full h-full flex lg:h-screen flex-wrap mx-auto lg:my-0">
+                <div className="z-30 absolute right-10 absolute bottom-7">
+                    <img className="lg:w-full" src="https://www.artwork.de/wp-content/uploads/2015/08/logo_TF_NEU_4c_ai.png" alt="IHKLogo"/>
+                </div>
+                <div className="z-20 w-full h-screen lg:w-4/12 mx-0 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-30">
+                    <div className="pl-4 pr-4 pb-2 pt-2 grid grid-rows-2 gap-8 mt-2">
+                        <div className="w-full h-0 pb-full"/>
+                        <SquareHolder/>
                     </div>
-                    <div className="w-full">
+                </div>
+                <div className="z-10 w-2/3 absolute right-0">
+                    <div className="pl-4 pr-4 pb-2 pt-2 grid grid-cols-2 gap-8 mt-2">
                         <SquareHolder />
-                    </div>
-                    <div className="w-full">
                         <SquareHolder />
-                    </div>
-                    <div className="w-full">
                         <SquareHolder />
-                    </div>
-                    <div className="w-full">
-                        <SquareHolder />
-                    </div>
-                    <div className="w-full">
                         <SquareHolder />
                     </div>
                 </div>
-                </div>
-            </div>;
-        </section>
+            </div>
+        </div>;
     }
 }
--- src/client/display_website/SquareHolder.tsx
+++ src/client/display_website/SquareHolder.tsx
@@ -1,29 +1,21 @@
 import * as React from "react";
-
 export class SquareHolder extends React.Component<any, any> {
 
     render() {
-        return <div className="bg-white rounded-lg overflow-hidden mb-10">
-            <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
-                <h3>
-                    <p
-                        className="
-                        font-semibold
-                        text-dark text-xl
-                        sm:text-[22px]
-                        md:text-xl
-                        lg:text-[22px]
-                        xl:text-xl
-                        2xl:text-[22px]
-                        mb-4
-                        block
-                        hover:text-primary
-                        "
-                    >
-                        widget componenent
-                    </p>
-                </h3>
-            </div>
-        </div>
+        return <div className="w-full h-0 shadow-2xl pb-full rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-70">
+                <div className="flex space-x-1 justify-center p-2" style={{
+                    opacity: 1
+                }}>
+                    <a href="#"
+                       className="px-1 py-1 rounded-full bg-gray-600 rounded-md hover:bg-gray-800 hover:text-white ">
+                    </a>
+                    <a href="#"
+                       className="px-1 py-1 rounded-full bg-gray-600 rounded-md hover:bg-gray-800 hover:text-white">
+                    </a>
+                    <a href="#"
+                       className="px-1 py-1 rounded-full bg-gray-600 rounded-md hover:bg-gray-800 hover:text-white">
+                    </a>
+                </div>
+        </div>;
     }
 }
