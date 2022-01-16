2482db6683eb3b6d56ff27297b8f7b56fb86f551
tom-mohr <tom-mohr@users.noreply.github.com>
2022-01-16 18:37:21 +0100

    added AnnouncementPersistence stub

---
+++ src/shared/persistence/AnnouncementPersistence.ts
@@ -0,0 +1,25 @@
+import {Announcement} from "../../server/announcement_management/Announcement";
+import {VerifiedUser} from "../values/VerifiedUser";
+
+export class AnnouncementPersistence {
+
+    setAnnouncements(announcements: Announcement[]) {
+        return [];
+    }
+
+    getAnnouncements(): Announcement[] {
+        return [];
+    }
+
+    getVerifiedUsers(): VerifiedUser[] {
+        return [];
+    }
+
+    addVerifiedUser(verifiedUser: VerifiedUser) {
+
+    }
+
+    removeVerifiedUser(verifiedUser: VerifiedUser) {
+
+    }
+}
