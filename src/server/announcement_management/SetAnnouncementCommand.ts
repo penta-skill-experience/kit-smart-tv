a5a30e518f0e3629935bc1d90727343756daf1d3
Aaron Siefker <aaron@familie-siefker.de>
2022-01-17 22:11:28 +0100

    added the necessary fields and a constructor to SetAnnouncementCommand.ts

--- src/server/announcement_management/SetAnnouncementCommand.ts
+++ src/server/announcement_management/SetAnnouncementCommand.ts
@@ -1,7 +1,18 @@
 import {AnnouncementCommand} from "./AnnouncementCommand";
+import {Announcement} from "./Announcement";
 
 export class SetAnnouncementCommand implements AnnouncementCommand {
-    
+
+    private announcement : Announcement
+
+    /**
+     * Constructs a SetAnnouncementCommand.
+     * @param announcement the announcement to execute this command for.
+     */
+    constructor(announcement : Announcement) {
+        this.announcement = announcement;
+    }
+
     executeCommand() {
         throw new Error("Method not implemented.");
     }
