cc2e505586b166fbfbec88219a7482dbbb6a1c3c
Aaron Siefker <aaron@familie-siefker.de>
2022-01-17 23:14:57 +0100

    wrote tests for the SetAnnouncementCommand

--- src/server/announcement_management/AnnouncementCommand.ts
+++ src/server/announcement_management/AnnouncementCommand.ts
@@ -16,3 +16,18 @@ export class AnnouncementCommandError extends Error {
         Object.setPrototypeOf(this, AnnouncementCommandError.prototype);
     }
 }
+
+/**
+ * this error class is used, if an announcement with an illegal text for that command is given in the constructor
+ * for an AnnouncementCommand.
+ *
+ * An error of this class is not meant to be caught.
+ */
+export class IllegalAnnouncementTextForCommandError extends Error {
+
+    constructor(msg : string) {
+        super(msg);
+
+        Object.setPrototypeOf(this, IllegalAnnouncementTextForCommandError.prototype);
+    }
+}
