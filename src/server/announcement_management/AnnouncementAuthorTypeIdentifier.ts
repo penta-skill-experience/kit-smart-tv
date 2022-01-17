317ab32e46961f9c4d3837d571a07434b3e0fe85
Aaron Siefker <aaron@familie-siefker.de>
2022-01-17 20:25:15 +0100

    implemented the stub for AnnouncementAuthorTypeIdentifier and wrote some tests for it

---
+++ src/server/announcement_management/AnnouncementAuthorTypeIdentifier.ts
@@ -0,0 +1,7 @@
+export class AnnouncementAuthorTypeIdentifier {
+
+    getAuthorType(author : string) : AnnouncementAuthorTypeIdentifier {
+        // TODO
+        throw new Error("not yet implemented");
+    }
+}
