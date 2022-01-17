d0084105467f552cc8b58dbee0223b885c275d6b
Aaron Siefker <aaron@familie-siefker.de>
2022-01-17 16:13:16 +0100

    removed the example emails again, as that can also be mocked using jest and implemented stubs for AnnouncementAuthorType

--- src/server/announcement_management/AnnouncementAuthorType.ts
+++ src/server/announcement_management/AnnouncementAuthorType.ts
@@ -1,5 +1,31 @@
-export enum AnnouncementAuthorType {
-    ADMIN,
-    VERIFIED,
-    UNVERIFIED
+export abstract class AnnouncementAuthorType {
+
+    values = [new AnnouncementAuthorType.ADMIN(), new AnnouncementAuthorType.VERIFIED(),
+              new AnnouncementAuthorType.UNVERIFIED];
+
+    static ADMIN = class extends AnnouncementAuthorType {
+
+        isThisAuthorType(author: string): Boolean {
+            // TODO
+            return false;
+        }
+    }
+
+    static VERIFIED = class extends AnnouncementAuthorType {
+
+        isThisAuthorType(author: string): Boolean {
+            // TODO
+            return false;
+        }
+    }
+
+    static UNVERIFIED = class extends AnnouncementAuthorType {
+
+        isThisAuthorType(author: string): Boolean {
+            // TODO
+            return false;
+        }
+    }
+
+    abstract isThisAuthorType(author : string) : Boolean;
 }
