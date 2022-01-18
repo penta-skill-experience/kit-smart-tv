c083711c60f64994699f068352201d183c4029a7
Lukas Xu <utvaz@student.kit.edu>
2022-01-18 15:27:07 +0100

    Added the grid layout to the personalization page and changed toggle buttons to radio buttons

--- src/client/config_website/PersonalizationPage.tsx
+++ src/client/config_website/PersonalizationPage.tsx
@@ -7,50 +7,45 @@ import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import meme1 from './mem.jpg'
 import meme2 from './mem2.jpg'
 import {useState} from "react";
+import {FormLabel, Grid} from "@mui/material";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
 
 
 
 export const PersonalizationPage= ({colorScheme, fontSize, handleColorSchemeChange, handleFontSizeChange, children}) => {
 
     return (
-        <>
-            <ToggleButtonGroup
-                value = {colorScheme}
-                exclusive
-                onChange = {handleColorSchemeChange}
-            >
-                <h1>Choose your color scheme:</h1>
-                <h1>light mode:</h1>
-                <ToggleButton value="light">
-                    <Brightness7Icon/>
-                </ToggleButton>
-                <h1>dark mode:</h1>
-                <ToggleButton value="dark">
-                    <Brightness4Icon/>
-                </ToggleButton>
-            </ToggleButtonGroup>
+        <div>
+            <Grid container spacing={2} direction="row" justifyContent="center" alignItems="center">
+                <Grid item xs={12}>
+                    <FormControl>
+                        <FormLabel>Choose your color scheme:</FormLabel>
+                        <RadioGroup onChange={handleColorSchemeChange} value={colorScheme}>
+                            <FormControlLabel value="light" control={<Radio />} label="light mode" />
+                            <FormControlLabel value="dark" control={<Radio />} label="dark mode" />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControl>
+                        <FormLabel>Choose your preferred font size:</FormLabel>
+                        <RadioGroup onChange={handleFontSizeChange} value={fontSize}>
+                            <FormControlLabel value="small" control={<Radio />} label="small" />
+                            <FormControlLabel value="medium" control={<Radio />} label="medium" />
+                            <FormControlLabel value="large" control={<Radio />} label="large" />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item>
+                    {renderBackground(colorScheme)}
+                </Grid>
 
-            <ToggleButtonGroup
-                value = {fontSize}
-                exclusive
-                onChange = {handleFontSizeChange}
-            >
-                <h1>Choose your preferred font size:</h1>
-                    <h1>Small:</h1>
-                    <ToggleButton value = "small">
-                        <RadioButtonCheckedIcon/>
-                    </ToggleButton>
-                    <h1>Medium:</h1>
-                    <ToggleButton value = "medium">
-                        <RadioButtonCheckedIcon/>
-                    </ToggleButton>
-                    <h1>Large:</h1>
-                    <ToggleButton value = "large">
-                        <RadioButtonCheckedIcon/>
-                    </ToggleButton>
-            </ToggleButtonGroup>
-            {renderBackground(colorScheme)}
-        </>
+
+            </Grid>
+        </div>
     );
 };
 
