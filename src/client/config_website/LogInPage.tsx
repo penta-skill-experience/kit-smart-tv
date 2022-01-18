7333f752c4fb0ee38597096f0ca28f3c6ab2fa87
Lukas Xu <utvaz@student.kit.edu>
2022-01-18 15:26:12 +0100

    Added the grid layout to the login page

--- src/client/config_website/LogInPage.tsx
+++ src/client/config_website/LogInPage.tsx
@@ -3,29 +3,35 @@ import {useState} from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import {Grid} from "@mui/material";
 
 export const LogInPage = ({logInInput, handleInput, handleLogIn, children}) => {
 
     return (
-        <Box
-            sx={{
-                width: 300,
-                height: 300,}
-            }
-        >
-            <div>
-                <h1>Log In Admin Interface</h1>
-                <TextField
-                    value={logInInput}
-                    label="Old Password"
-                    onChange={handleInput}
-                />
-                <Button
-                    onClick={handleLogIn}
-                    variant="outlined"
-                >
-                    Log In</Button>
-            </div>
-        </Box>
+        <div>
+            <Grid container spacing={2} direction="column" justifyContent="center" alignItems="center">
+                <Grid item xs={12}>
+                    <h1 className="text-lg">Log In Admin Interface</h1>
+                </Grid>
+                <Grid item>
+                    <TextField
+                        value={logInInput}
+                        label="Enter Password"
+                        onChange={handleInput}
+                    />
+                </Grid>
+                <Grid item>
+                    <Button
+                        onClick={handleLogIn}
+                        variant="outlined"
+                    >
+                        Log In</Button>
+                </Grid>
+            </Grid>
+        </div>
+
+
+
+
     );
 }
