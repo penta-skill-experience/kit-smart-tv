2f9c04e6921ae47e52476907b31187f305c54573
Lukas Xu <utvaz@student.kit.edu>
2022-01-18 17:55:47 +0100

    Added the buttons for saving the changes and logout

--- src/client/config_website/AdminPage.tsx
+++ src/client/config_website/AdminPage.tsx
@@ -4,7 +4,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {Grid} from "@mui/material";
 
-export const AdminPasswordPage = ({oldPassword, newPassword, handleOldPassword, handleNewPassword, handlePasswordChange, children} ) => {
+export const AdminPage = ({oldPassword, newPassword, handleOldPassword, handleNewPassword, handlePasswordChange, handleChangeSave, handleLogout, children} ) => {
 
     return (
         <Box
@@ -36,6 +36,20 @@ export const AdminPasswordPage = ({oldPassword, newPassword, handleOldPassword, 
                             variant="outlined"
                         >
                             Save Password</Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button
+                            onClick={handleChangeSave}
+                            variant="outlined"
+                        >
+                            Save Changes</Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button
+                            onClick={handleLogout}
+                            variant="outlined"
+                        >
+                            Log out</Button>
                     </Grid>
                 </Grid>
             </div>
