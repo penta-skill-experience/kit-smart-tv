ab9b7a59f25f679d1f5d62622cc892a63062f469
Lukas Xu <utvaz@student.kit.edu>
2022-01-18 15:25:47 +0100

    Added the grid layout to the admin password change page

--- src/client/config_website/AdminPasswordPage.tsx
+++ src/client/config_website/AdminPasswordPage.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import {Grid} from "@mui/material";
 
 export const AdminPasswordPage = ({oldPassword, newPassword, handleOldPassword, handleNewPassword, handlePasswordChange, children} ) => {
 
@@ -14,21 +15,29 @@ export const AdminPasswordPage = ({oldPassword, newPassword, handleOldPassword, 
         }
         >
             <div>
-                <TextField
-                    value={oldPassword}
-                    label="Old Password"
-                    onChange={handleOldPassword}
-                />
-                <TextField
-                    value={newPassword}
-                    label="New Password"
-                    onChange={handleNewPassword}
-                />
-                <Button
-                    onClick={handlePasswordChange}
-                    variant="outlined"
-                >
-                    Save Password</Button>
+                <Grid container spacing={2} direction="row" justifyContent="center" alignItems="center">
+                    <Grid item xs={12}>
+                        <TextField
+                            value={oldPassword}
+                            label="Old Password"
+                            onChange={handleOldPassword}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            value={newPassword}
+                            label="New Password"
+                            onChange={handleNewPassword}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button
+                            onClick={handlePasswordChange}
+                            variant="outlined"
+                        >
+                            Save Password</Button>
+                    </Grid>
+                </Grid>
             </div>
         </Box>
     );
