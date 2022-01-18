9eaf82b2ebbf50d38663f46c485b926d7eb83435
Lukas Xu <utvaz@student.kit.edu>
2022-01-18 15:27:47 +0100

    Added the grid layout to the layout page

--- src/client/config_website/LayoutPage.tsx
+++ src/client/config_website/LayoutPage.tsx
@@ -3,30 +3,76 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import {Button} from "@mui/material";
+import {Button, Checkbox, Grid} from "@mui/material";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export const LayoutPage = ({list, widget, handleWidgetSelection, handleAddWidget, children}) => {
 
     return(
         <div>
-            <ul>
-                {list.map(item => (
-                    <li key={item}>{item}</li>
-                ))}
-            </ul>
-            <FormControl sx={{minWidth: 120}}>
-                <Select
-                    value={widget}
-                    onChange={handleWidgetSelection}
-                >
-                    //todo
-                    //Add widgets
-                    <MenuItem value={'test1'}>Test1</MenuItem>
-                    <MenuItem value={'test2'}>Test2</MenuItem>
-                    <MenuItem value={'test3'}>Test3</MenuItem>
-                </Select>
-            </FormControl>
-            <Button onClick={handleAddWidget}>Add Widget</Button>
+            <Grid container spacing={2} direction="column" justifyContent="flex-start" alignItems="flex-start">
+                <Grid item>
+                    <ul>
+                        {list.map(widget => (
+                            <Grid container spacing={2} direction="row" alignItems="center">
+                                <Grid item>
+                                    {widget}
+                                </Grid>
+                                <Grid item>
+                                    <FormGroup>
+                                        <FormControlLabel control={<Checkbox />} label="1" />
+                                    </FormGroup>
+                                </Grid>
+                                <Grid item>
+                                    <FormGroup>
+                                        <FormControlLabel control={<Checkbox />} label="2" />
+                                    </FormGroup>
+                                </Grid>
+                                <Grid item>
+                                    <FormGroup>
+                                        <FormControlLabel control={<Checkbox />} label="3" />
+                                    </FormGroup>
+                                </Grid>
+                                <Grid item>
+                                    <FormGroup>
+                                        <FormControlLabel control={<Checkbox />} label="4" />
+                                    </FormGroup>
+                                </Grid>
+                                <Grid item>
+                                    <FormGroup>
+                                        <FormControlLabel control={<Checkbox />} label="5" />
+                                    </FormGroup>
+                                </Grid>
+                                <Grid item>
+                                    <Button>
+                                        <DeleteIcon/>
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            //<li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </Grid>
+                <Grid item>
+                    <FormControl sx={{minWidth: 120}}>
+                        <Select
+                            value={widget}
+                            onChange={handleWidgetSelection}
+                        >
+                            //todo
+                            //Add widgets
+                            <MenuItem value={'test1'}>Test1</MenuItem>
+                            <MenuItem value={'test2'}>Test2</MenuItem>
+                            <MenuItem value={'test3'}>Test3</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item>
+                    <Button onClick={handleAddWidget} variant="outlined">Add Widget</Button>
+                </Grid>
+            </Grid>
         </div>
     );
 };
