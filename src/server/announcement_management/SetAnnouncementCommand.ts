3d3e933d3007859ee42427e500fa6f57039dfba4
Aaron Siefker <aaron@familie-siefker.de>
2022-01-18 02:26:45 +0100

    implemented the add announcement part of SetAnnouncementCommand

--- src/server/announcement_management/SetAnnouncementCommand.ts
+++ src/server/announcement_management/SetAnnouncementCommand.ts
@@ -13,8 +13,11 @@ export class SetAnnouncementCommand implements AnnouncementCommand {
     private static readonly INVALID_ANNOUNCEMENT_TEXT = "";
     private static readonly INVALID_ANNOUNCEMENT_TEXT_ERROR_MSG = "Error, the announcement text for this command " +
         "cannot be an empty string";
-    private static readonly THIS_AUTHOR_CANNOT_EDIT_THIS_ANNOUNCEMENT = "Error, this announcement author cannot " +
-                                                                        "edit this announcement."
+    private static readonly THIS_AUTHOR_CANNOT_EDIT_THIS_ANNOUNCEMENT_ERROR_MSG = "Error, this announcement author cannot " +
+        "edit this announcement.";
+    private static readonly THIS_AUTHOR_NOT_ALLOWED_TO_ADD_ANN_ERROR_MSG = "Error, this announcement author is not " +
+        "allowed to add announcements";
+
 
     private announcement : Announcement
 
@@ -52,19 +55,27 @@ export class SetAnnouncementCommand implements AnnouncementCommand {
 
         if (!(announcementToEdit.author === this.announcement.author
             || authorType.isAllowedToEditAnnouncementsFromOtherAuthors())) {
-            throw new AnnouncementCommandError(SetAnnouncementCommand.THIS_AUTHOR_CANNOT_EDIT_THIS_ANNOUNCEMENT);
+            throw new AnnouncementCommandError(SetAnnouncementCommand.THIS_AUTHOR_CANNOT_EDIT_THIS_ANNOUNCEMENT_ERROR_MSG);
         }
-        const announcementsToSet = [...currentAnnouncements];
-        const indexToRemove = announcementsToSet.indexOf(announcementToEdit, 0);
+        const announcementsToSend = [...currentAnnouncements];
+        const indexToRemove = announcementsToSend.indexOf(announcementToEdit, 0);
         if (indexToRemove > -1) {
-            announcementsToSet.splice(indexToRemove, 1);
+            announcementsToSend.splice(indexToRemove, 1);
         }
-        announcementsToSet.push(this.announcement);
-        new AnnouncementPersistence().setAnnouncements(announcementsToSet);
+        this.pushAnnouncementAndSendToPersistence(announcementsToSend);
     }
 
     private addAnnouncement(authorType : AnnouncementAuthorType, currentAnnouncements : Announcement[]) {
+        if (!authorType.isAllowedToAddAnnouncement()) {
+            throw new AnnouncementCommandError(SetAnnouncementCommand.THIS_AUTHOR_NOT_ALLOWED_TO_ADD_ANN_ERROR_MSG);
+        }
+        this.pushAnnouncementAndSendToPersistence(currentAnnouncements);
+    }
 
+    private pushAnnouncementAndSendToPersistence(announcements : Announcement[]) {
+        const announcementsToSend = [...announcements];
+        announcementsToSend.push(this.announcement);
+        new AnnouncementPersistence().setAnnouncements(announcementsToSend);
     }
 
 }
