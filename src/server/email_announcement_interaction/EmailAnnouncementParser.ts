24c3e5f12fa8525c5a7fd7b3eb4870e83bdecf91
Aaron Siefker <aaron@familie-siefker.de>
2022-01-19 11:15:22 +0100

    installed a mail-listener library for typescript and set up the stubs for EmailAnnouncementParser.ts

---
+++ src/server/email_announcement_interaction/EmailAnnouncementParser.ts
@@ -0,0 +1,11 @@
+import {AnnouncementParser} from "../announcement_management/AnnouncementParser";
+import {Announcement} from "../announcement_management/Announcement";
+
+export class EmailAnnouncementParser implements AnnouncementParser {
+
+    parseToAnnouncement(): Announcement {
+        // TODO
+        throw new Error("not yet implemented");
+    }
+
+}
