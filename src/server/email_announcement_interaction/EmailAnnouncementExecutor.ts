4e666836b0da6470c683fcded978536b133ff5b5
Aaron Siefker <aaron@familie-siefker.de>
2022-01-19 17:51:21 +0100

    implemented the stubs for EmailAnnouncementExecutor and created the test file for it

---
+++ src/server/email_announcement_interaction/EmailAnnouncementExecutor.ts
@@ -0,0 +1,8 @@
+import {IMailObject} from "mail-listener-typescript";
+
+export class EmailAnnouncementExecutor {
+
+    executeEmailCommand(mail : IMailObject) {
+        throw new Error("not yet implemented");
+    }
+}
