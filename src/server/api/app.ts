e9858010619e4ab089553956f4b50f5b78ce4af6
tom-mohr <tom-mohr@users.noreply.github.com>
2022-01-19 03:28:27 +0100

    host static client files under "/" and "/admin-interface"

--- src/server/api/app.ts
+++ src/server/api/app.ts
@@ -9,14 +9,21 @@ import connect from './utils/conntectDb';
 import validate from "./middleware/validateResource";
 import {createAdminSchema} from "./schema/admin.schema";
 import {createAdminHandler} from "./controller/admin.controller";
+import path from "path";
 
 const port = config.port;
 
 const app = express();
- app.use(express.json());
+app.use(express.json());
 
-app.listen(port, async () =>{
-    console.log(`this app is running at http://localhost:${ port }`);
+// host static files of display_website and config_website
+app.use("/",
+    express.static(path.resolve(__dirname, "..", "display_website")));
+app.use("/admin-interface",
+    express.static(path.resolve(__dirname, "..", "config_website")));
+
+app.listen(port, async () => {
+    console.log(`this app is running at http://localhost:${port}`);
     await connect();
 
     app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
