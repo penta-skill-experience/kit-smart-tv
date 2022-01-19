5cef8470e92cc2d642177f066ddd3d626150b074
Aaron Siefker <aaron@familie-siefker.de>
2022-01-19 18:20:04 +0100

    implemented executeEmailCommand for EmailAnnouncementExecutor

--- src/server/email_announcement_interaction/EmailAnnouncementExecutor.ts
+++ src/server/email_announcement_interaction/EmailAnnouncementExecutor.ts
@@ -1,8 +1,21 @@
 import {IMailObject} from "mail-listener-typescript";
+import {EmailAnnouncementParser} from "./EmailAnnouncementParser";
+import {EmailAnnouncementCommandIdentifier} from "./EmailAnnouncementCommandIdentifier";
+import {AnnouncementCommandError} from "../announcement_management/AnnouncementCommand";
 
 export class EmailAnnouncementExecutor {
 
     executeEmailCommand(mail : IMailObject) {
-        throw new Error("not yet implemented");
+        const parsedAnnouncement = new EmailAnnouncementParser(mail).parseToAnnouncement();
+
+        try {
+            new EmailAnnouncementCommandIdentifier(parsedAnnouncement).identifyCommand().executeCommand();
+        } catch(e) {
+            if (e instanceof AnnouncementCommandError) {
+                // can later be used to send feedback
+            } else {
+                throw e;
+            }
+        }
     }
 }
