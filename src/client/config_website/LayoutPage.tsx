6103a59022b92d9f4e3a46a32e0cc86235e6e7a5
Lukas Xu <utvaz@student.kit.edu>
2022-01-20 13:47:35 +0100

    Made grid to visualize the position on the screen for the widgets

--- src/client/config_website/LayoutPage.tsx
+++ src/client/config_website/LayoutPage.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import {Button, Checkbox, Grid} from "@mui/material";
-import FormGroup from '@mui/material/FormGroup';
+import Select from '@mui/material/Select';
+import {Button, Grid} from "@mui/material";
 import FormControlLabel from '@mui/material/FormControlLabel';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import LockIcon from '@mui/icons-material/Lock';
 
 
 export const LayoutPage = ({list, widget, handleWidgetSelection, handleAddWidget, handleDeleteWidget, handlePosition, children}) => {
@@ -16,6 +17,143 @@ export const LayoutPage = ({list, widget, handleWidgetSelection, handleAddWidget
     return(
         <div>
             <Grid container spacing={2} direction="column" justifyContent="flex-start" alignItems="flex-start">
+                <Grid item container spacing={2} direction="row">
+                    <Grid item>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                backgroundColor: 'text.primary',
+                            }
+                            }
+                        >
+                            <Grid container direction="row" alignItems="center" justifyContent="space-between">
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item  container alignItems="center" justifyContent="center">
+                                    <Grid item>
+                                        <LockIcon color='error'/>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Box>
+
+                    </Grid>
+
+                    <Grid item>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                backgroundColor: 'text.primary',
+                                }
+                            }
+                        >
+                            <Grid container direction="row" alignItems="center" justifyContent="space-between">
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item  container alignItems="center" justifyContent="center">
+                                    <Grid item>
+                                        <Typography variant="h6" color='white'>1.top center</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Box>
+
+                    </Grid>
+
+                    <Grid item>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                backgroundColor: 'text.primary',
+                            }
+                            }
+                        >
+                            <Grid container direction="row" alignItems="center" justifyContent="space-between">
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item  container alignItems="center" justifyContent="center">
+                                    <Grid item>
+                                        <Typography variant="h6" color='white'>2.top right</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Box>
+
+                    </Grid>
+
+                </Grid>
+
+                <Grid item container spacing={2} direction="row">
+                    <Grid item>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                backgroundColor: 'text.primary',
+                            }
+                            }
+                        >
+                            <Grid container direction="row" alignItems="center" justifyContent="space-between">
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item  container alignItems="center" justifyContent="center">
+                                    <Grid item>
+                                        <Typography variant="h6" color='white'>3.bottom left</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Box>
+
+                    </Grid>
+
+                    <Grid item>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                backgroundColor: 'text.primary',
+                            }
+                            }
+                        >
+                            <Grid container direction="row" alignItems="center" justifyContent="space-between">
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item  container alignItems="center" justifyContent="center">
+                                    <Grid item>
+                                        <Typography variant="h6" color='white'>4.bottom center</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Box>
+
+                    </Grid>
+
+                    <Grid item>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                backgroundColor: 'text.primary',
+                            }
+                            }
+                        >
+                            <Grid container direction="row" alignItems="center" justifyContent="space-between">
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item xs={12}>''</Grid>
+                                <Grid item  container alignItems="center" justifyContent="center">
+                                    <Grid item>
+                                        <Typography variant="h6" color='white'>5.bottom right</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Box>
+
+                    </Grid>
+                </Grid>
+
                 <Grid item>
                     <ul>
                         {list.map(widget => (
