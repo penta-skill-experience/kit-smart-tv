e03c6dc264205a7af448e9f098ee8b20f43dc557
tom-mohr <tom-mohr@users.noreply.github.com>
2022-01-21 18:10:45 +0100

    WidgetPersistence.ts returns promise

--- src/shared/persistence/WidgetPersistence.ts
+++ src/shared/persistence/WidgetPersistence.ts
@@ -6,8 +6,26 @@ export class WidgetPersistence {
         //todo
     }
 
-    getWidgetDataList(): WidgetData[] {
-        //todo
-        return [];
+    getWidgetDataList(): Promise<WidgetData[]> {
+        //todo: this is just a mock
+        return new Promise<WidgetData[]>(resolve => {
+
+                // insert data for widgets that you want to test here:
+                const data = [
+                    {
+                        widgetId: "tram-schedule",
+                        location: 1,
+                        rawConfig: {},
+                    },
+                    {
+                        widgetId: "rss-feed",
+                        location: 3,
+                        rawConfig: {url: "www.example.com"},
+                    },
+                ];
+
+                resolve(data);
+            }
+        );
     }
 }
