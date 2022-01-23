1207e0764ce0b0b3e75b7a8af5dba1bca61e9d82
Lukas Xu <utvaz@student.kit.edu>
2022-01-23 12:53:19 +0100

    Modified the text-fields in the admin page to hide what's typed in.

--- src/client/config_website/AdminPage.tsx
+++ src/client/config_website/AdminPage.tsx
@@ -3,8 +3,25 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {Grid} from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import Input from "@mui/material/Input";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Visibility from "@mui/icons-material/Visibility";
+import FormControl from "@mui/material/FormControl";
 
 export const AdminPage = ({oldPassword, newPassword, handleOldPassword, handleNewPassword, handlePasswordChange, handleChangeSave, children} ) => {
+    const [showOldPassword, setShowOldPassword] = React.useState(false);
+    const [showNewPassword, setShowNewPassword] = React.useState(false);
+
+    const handleShowOldPassword  = () => {
+        setShowOldPassword(!showOldPassword)
+    }
+
+    const handleShowNewPassword  = () => {
+        setShowNewPassword(!showNewPassword)
+    }
 
     return (
         <Box
@@ -17,18 +34,42 @@ export const AdminPage = ({oldPassword, newPassword, handleOldPassword, handleNe
             <div>
                 <Grid container spacing={2} direction="row" justifyContent="center" alignItems="center">
                     <Grid item xs={12}>
-                        <TextField
-                            value={oldPassword}
-                            label="Old Password"
-                            onChange={handleOldPassword}
-                        />
+                        <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+                            <InputLabel>Old Password</InputLabel>
+                            <Input
+                                type={showOldPassword ? 'text' : 'password'}
+                                value={oldPassword}
+                                onChange={handleOldPassword}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            onClick={handleShowOldPassword}
+                                        >
+                                            {showOldPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl>
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField
-                            value={newPassword}
-                            label="New Password"
-                            onChange={handleNewPassword}
-                        />
+                        <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+                            <InputLabel>New Password</InputLabel>
+                                <Input
+                                type={showNewPassword ? 'text' : 'password'}
+                                value={newPassword}
+                                onChange={handleNewPassword}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                        onClick={handleShowNewPassword}
+                                        >
+                                        {showNewPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                />
+                        </FormControl>
                     </Grid>
                     <Grid item xs={12}>
                         <Button
@@ -48,5 +89,4 @@ export const AdminPage = ({oldPassword, newPassword, handleOldPassword, handleNe
             </div>
         </Box>
     );
-
 }
