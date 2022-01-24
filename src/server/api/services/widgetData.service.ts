b0c7e2609733b9a932ae47cedcae1025a11d2be2
jannikbach <jannik.bach@icloud.com>
2022-01-24 07:33:04 +0100

    fix API responses

--- src/server/api/services/widgetData.service.ts
+++ src/server/api/services/widgetData.service.ts
@@ -2,17 +2,19 @@ import {DocumentDefinition} from "mongoose";
 import {WidgetDataInput, WidgetDataModel} from "../models/widgetDataModel";
 import {omit} from "lodash";
 
-export function createWidgetData(input: DocumentDefinition<WidgetDataInput>) {
+export async function createWidgetData(input: DocumentDefinition<WidgetDataInput>) {
     //before creating a widget data list, delete all widget data list in the collection top guarantee there ist only one stored at a time
     //delete all
-    WidgetDataModel.remove({});
-    return WidgetDataModel.create(input).then(o => omit(o.toJSON(), ["_id", "createdAt", "updatedAt", "__v"]));
+    await WidgetDataModel.remove({});
+    await WidgetDataModel.create(input);
+    return await WidgetDataModel.findOne({}).then(o => omit(o.toJSON(), ["_id", "createdAt", "updatedAt", "__v"]));
 }
 
-export function updateWidgetData(input: DocumentDefinition<WidgetDataInput>) {
-    return WidgetDataModel.findOneAndUpdate({}, input).then(o => omit(o.toJSON(), ["_id", "createdAt", "updatedAt", "__v"]));
+export async function updateWidgetData(input: DocumentDefinition<WidgetDataInput>) {
+    await WidgetDataModel.findOneAndUpdate({}, input);
+    return await WidgetDataModel.findOne({}).then(o => omit(o.toJSON(), ["_id", "createdAt", "updatedAt", "__v"]));
 }
 
-export function getWidgetData() {
-    return WidgetDataModel.findOne({}).then(o => omit(o.toJSON(), ["_id", "createdAt", "updatedAt", "__v"]));
+export async function getWidgetData() {
+    return await WidgetDataModel.findOne({}).then(o => omit(o.toJSON(), ["_id", "createdAt", "updatedAt", "__v"]));
 }
