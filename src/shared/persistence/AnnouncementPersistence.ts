ac1cda80f915882c61ac36ce4e0fe0d0dd2f3b7d
jannikbach <jannik.bach@icloud.com>
2022-01-24 19:32:19 +0100

    fixed update token bug

--- src/shared/persistence/AnnouncementPersistence.ts
+++ src/shared/persistence/AnnouncementPersistence.ts
@@ -1,5 +1,6 @@
 import {Announcement} from "../../server/announcement_management/Announcement";
 import {VerifiedUser} from "../values/VerifiedUser";
+import {unirest} from "unirest";
 
 export class AnnouncementPersistence {
 
@@ -8,6 +9,21 @@ export class AnnouncementPersistence {
     }
 
     getAnnouncements(): Announcement[] {
+        let accessToken: string;
+        let refreshToken: string;
+        var req = unirest('GET', 'http://localhost:80/announcements')
+            .headers({
+                'x-refresh': refreshToken,
+                'Authorization': 'Bearer ' + accessToken
+            })
+            .end(function (res) {
+                if (res.error) throw new Error(res.error);
+                console.log(res.raw_body);
+
+
+            });
+
+
         //todo
         return [];
     }
