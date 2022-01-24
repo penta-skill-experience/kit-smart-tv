f144b5c6bc981794f4fac814dc2b15ea7b6cdcbb
Morris BE <ubsmd@student.kit.edu>
2022-01-24 02:16:48 +0100

    Made dots display at the bottom of widgets (only those that dynamically rotate) to indicate which rotation a user is on.

--- src/client/display_website/RotatorComponent.tsx
+++ src/client/display_website/RotatorComponent.tsx
@@ -2,18 +2,23 @@ import * as React from "react";
 import * as RotatorComponentConfig from "./RotatorComponent.json";
 
 export class RotatorComponent extends React.Component<any, any> {
-
-    pageSwitch = function() {
+    setLength = function () {
+        this.state.elements = this.props.children.length;
+    }
+    pageSwitch = function () {
         this.setState({index: (this.state.index + 1)});
-        if(this.state.index >= this.props.children.length) {
+        if (this.state.index >= this.state.elements) {
             this.setState({index: 0});
         }
     }
+
     constructor(props) {
         super(props);
         this.state = {
-            index: 0
+            index: 0,
+            elements: 0
         };
+        this.setLength();
         this.pageSwitch = this.pageSwitch.bind(this);
         setInterval(() => this.pageSwitch(), RotatorComponentConfig.SWITCH_SPEED);
     }
@@ -22,10 +27,17 @@ export class RotatorComponent extends React.Component<any, any> {
         return <div>
             {React.Children.map(this.props.children, (child, i) =>
                 <div>
-                    {((this.state.index == i) ? child:"")}
-
+                    {((this.state.index == i) ? child : "")}
                 </div>
             )}
+            {(this.state.elements > 1) ?
+                <div className="flex space-x-1 justify-center"> {React.Children.map(this.props.children, (child, k) =>
+                    <a href="#"
+                       className={"w-4 h-4 rounded-full rounded-md " + ((this.state.index == k) ? "bg-gray-900" : "bg-gray-500")}>
+                    </a>
+                )}
+                </div>
+                : ""}
         </div>
     }
 }
