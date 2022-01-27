10ed37500eeba7081a0a61536f11827270d8b7d9
tom-mohr <tom-mohr@users.noreply.github.com>
2022-01-27 11:49:16 +0100

    fixed reading stop name in Tram Schedule widget

--- src/client/widget_catalog/tram_schedule/TramScheduleDisplayComponent.tsx
+++ src/client/widget_catalog/tram_schedule/TramScheduleDisplayComponent.tsx
@@ -9,11 +9,6 @@ interface DepartureData {
     time: string;
 }
 
-interface TramScheduleState {
-    trains: DepartureData[];
-    stop: string;
-}
-
 let justArrived = (x: string): string => {
     if (/^-?\d+$/.test(x) && Number(x) == 0) {
         return "now!"
@@ -21,50 +16,79 @@ let justArrived = (x: string): string => {
     return x;
 };
 
+interface TramScheduleState {
+    trains: DepartureData[];
+    stopId: string | undefined;
+}
+
 export class TramScheduleDisplayComponent extends DisplayComponent<TramScheduleState> {
     constructor(props) {
         super(props);
         this.state = {
             trains: [],
-            stop: this.props.config["stop"]
+            stopId: undefined,
         };
+        console.log("TramScheduleDisplayComponent received the following config:");
+        console.log(this.props.config);
     }
-    getSchedule() {
-        axios.get("http://localhost:8080/" + TramScheduleConfig.URL_STOP_SEARCH_BEFORE_STOP
-            + this.state.stop
-            + TramScheduleConfig.URL_STOP_SEARCH_AFTER_STOP
-            + TramScheduleConfig.API_KEY)
-            .then(resp => {
-                let checker = resp.data.stops;
-                if(checker.length == 0) {
-                    throw new Error(`This stop does not exist`);
-                }
-                axios.get(TramScheduleConfig.CORS_ANYWHERE
-                    + TramScheduleConfig.URL_BEFORE_STOP
-                    + checker[0].id
-                    + TramScheduleConfig.URL_AFTER_STOP
-                    + TramScheduleConfig.API_KEY)
-                    .then(resp => {
-                        console.log(checker[0].id);
-                        this.setState({
-                            trains: resp.data.departures.map(d => ({
-                                route: d.route,
-                                destination: d.destination,
-                                time: justArrived(d.time)
-                            })),
-                            stop: checker[0].id
-                        });
+
+    private getStopName(): string {
+        return this.props.config["stop"]["label"];
+    }
+
+    private querySchedule() {
+        if (this.state.stopId === undefined) {
+            // get stopId before querying departure data
+
+            const stopName = this.getStopName();
+
+            let url = "http://localhost:8080/" + TramScheduleConfig.URL_STOP_SEARCH_BEFORE_STOP
+                + stopName
+                + TramScheduleConfig.URL_STOP_SEARCH_AFTER_STOP
+                + TramScheduleConfig.API_KEY;
+
+            axios.get(url)
+                .then(resp => {
+                    let checker = resp.data.stops;
+                    if (checker.length == 0) {
+                        throw new Error(`The stop ${stopName} does not exist`);
+                    }
+                    let stopId = checker[0].id;
+                    this.setState({
+                        stopId: stopId,
                     });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+                });
+
+        } else {
+            // immediately query departure data
+            this.queryDepartureData();
+        }
     };
 
-    componentDidMount() {
-        this.getSchedule();
-        setInterval(() => this.getSchedule(), TramScheduleConfig.REFRESH_RATE);
+    private queryDepartureData() {
+        axios.get(TramScheduleConfig.CORS_ANYWHERE
+            + TramScheduleConfig.URL_BEFORE_STOP
+            + this.state.stopId
+            + TramScheduleConfig.URL_AFTER_STOP
+            + TramScheduleConfig.API_KEY)
+            .then(resp => {
+                console.log(this.state.stopId);
+                this.setState({
+                    trains: resp.data.departures.map(d => ({
+                        route: d.route,
+                        destination: d.destination,
+                        time: justArrived(d.time)
+                    })),
+                    stop: this.state.stopId
+                });
+            });
     }
+
+    componentDidMount() {
+        this.querySchedule();
+        setInterval(() => this.querySchedule(), TramScheduleConfig.REFRESH_RATE);
+    }
+
     render() {
         return <div className="grid grid-flow-row sm:g-0.5 xl:gap-1.5 2xl:gap-2 box-border">
             {
@@ -76,8 +100,8 @@ export class TramScheduleDisplayComponent extends DisplayComponent<TramScheduleS
             }
             {
                 this.state.trains.slice(1, TramScheduleConfig.ITEM_COUNT).map((d, index) =>
-                    <div key = {index}
-                        className="font-light leading-normal sm:text-xs lg:text-base xl:text-base 2xl:text-xl 4xl:text-2xl sm:text-left 8xl:text-4xl">
+                    <div key={index}
+                         className="font-light leading-normal sm:text-xs lg:text-base xl:text-base 2xl:text-xl 4xl:text-2xl sm:text-left 8xl:text-4xl">
                         {d.route} {d.destination}: &nbsp;&nbsp;&nbsp;{d.time}
                     </div>
                 )
