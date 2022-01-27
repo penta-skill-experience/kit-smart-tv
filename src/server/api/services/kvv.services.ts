77b69f5d696de09806cfbb54fbfb4a0c8b401c7a
jannikbach <jannik.bach@icloud.com>
2022-01-27 22:17:23 +0100

    bugfixing

--- src/server/api/services/kvv.services.ts
+++ src/server/api/services/kvv.services.ts
@@ -1,12 +1,12 @@
 import {DocumentDefinition} from "mongoose";
 import {KvvDocument} from "../models/kvv.model";
-import {exec} from 'child_process';
+import {execSync} from 'child_process';
 import * as fs from 'fs';
 
 
 export async function getKvv(input: DocumentDefinition<KvvDocument>){
     const command = `rm ../kvv.json | curl -o ../kvv.json "${input.url}"`
-    await exec(command, () => {});
+    await execSync(command);
     let rawData = fs.readFileSync('../kvv.json');
     return JSON.parse(rawData.toString());
 }
