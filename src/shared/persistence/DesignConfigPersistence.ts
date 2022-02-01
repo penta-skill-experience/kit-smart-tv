4a825182db608acffc8ca0096e300cc44b206c03
Aaron <uupiw@student.kit.edu>
2022-02-01 20:07:44 +0100

    implemented DesignConfigPersistenceSetters

--- src/shared/persistence/DesignConfigPersistence.ts
+++ src/shared/persistence/DesignConfigPersistence.ts
@@ -1,5 +1,6 @@
 import {ConfigData} from "./data";
 import config from "./persistence.config.json";
+import {TokenHolderSingleton} from "./TokenHolderSingleton";
 
 export class DesignConfigPersistence {
 
@@ -16,15 +17,30 @@ export class DesignConfigPersistence {
     }
 
     setSelectedColorSchemeId(id: string) {
-        //todo
+        return this.getConfigData()
+            .then(configData => {
+                const configToSet = configData;
+                configToSet.colorScheme = id;
+                return this.setConfigData(configToSet)
+            })
     }
 
     setSelectedBackground(id: string) {
-        //todo
+        return this.getConfigData()
+            .then(configData => {
+                const configToSet = configData;
+                configToSet.background = id;
+                return this.setConfigData(configToSet)
+            });
     }
 
     setSelectedFontSize(id: string) {
-        //todo
+        return this.getConfigData()
+            .then(configData => {
+                const configToSet = configData;
+                configToSet.fontSize = id;
+                return this.setConfigData(configToSet)
+            });
     }
 
     /* Queries the whole config object.
@@ -33,5 +49,37 @@ export class DesignConfigPersistence {
     getConfigData(): Promise<ConfigData> {
         return fetch(`${config.DOMAIN}/config`)
             .then((value: Response) => value.json());
+    }
+
+    setConfigData(configData : ConfigData) {
+
+        const headers = new Headers();
+        headers.append("x-refresh", TokenHolderSingleton.instance.refreshToken);
+        headers.append("Authorization", `Bearer ${sessionStorage.getItem("accessToken")}`);
+        headers.append("Content-Type", "application/json");
+
+        const requestOptions = {
+            method: 'PUT',
+            headers: headers,
+            body: JSON.stringify(configData),
+        };
+
+        return new Promise<void>((resolve, reject) => {
+            fetch(`${config.DOMAIN}/config`, requestOptions)
+                .then(response => {
+                    const new_accessToken = response.headers.get('x-access-token');
+                    if (new_accessToken) {
+                        //if a new accessToken is provided, update it.
+                        sessionStorage.setItem("accessToken", new_accessToken);
+                    }
+
+                    if (response.status === 200) {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                })
+                .catch(() => reject());
+        })
     }
 }
