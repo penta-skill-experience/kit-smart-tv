410ab1ce866468f1121b812572095eb4f3ffd75e
Morris BE <ubsmd@student.kit.edu>
2022-02-02 05:15:12 +0100

    Fixed rounding issue for weather values.

--- src/client/widget_catalog/weather/WeatherDisplayComponent.tsx
+++ src/client/widget_catalog/weather/WeatherDisplayComponent.tsx
@@ -30,10 +30,10 @@ export class WeatherDisplayComponent extends DisplayComponent<any> {
     getWeather() {
         axios.get(WeatherConfig.URL + WeatherConfig.API_KEY)
             .then(resp => this.setState({
-                temp: resp.data.current.temp,
-                humidity: resp.data.current.humidity,
-                windSpeed: resp.data.current.wind_speed,
-                precipitation: resp.data.hourly[0].pop,
+                temp: Math.floor(resp.data.current.temp),
+                humidity: Math.floor(resp.data.current.humidity),
+                windSpeed: Math.floor(toKMH(resp.data.current.wind_speed)),
+                precipitation: Math.floor(resp.data.hourly[0].pop  * 100),
                 icon: resp.data.current.weather[0].icon,
                 gotIcon: true,
             }))
@@ -44,7 +44,7 @@ export class WeatherDisplayComponent extends DisplayComponent<any> {
 
     render() {
         return (this.state.gotIcon ? <div className="grid grid-col-3 grid-flow-col gap-3 box-border items-center sm:pt-4 xl:pt-12 2xl:pt-16 8xl:pt-20 h-fit">
-                <img className="sm:w-8 md:w-10 lg:w-16 xl:w-20 4xl:w-28 8xl:w-40 sm:object-left sm:scale-150 lg:scale-150" src={WeatherConfig.URL_ICONS + this.state.icon + "@4x.png"} alt="logo"/>
+                <img className="sm:w-8 md:w-10 lg:w-16 xl:w-20 4xl:w-28 8xl:w-40 sm:object-left sm:scale-150 lg:scale-150" src={WeatherConfig.URL_ICONS + this.state.icon + WeatherConfig.URL_ICONS_POST} alt="logo"/>
                 <div className="justify-center">
                     <div className="font-light leading-normal sm:text-base xl:text-xl 2xl:text-3xl 4xl:text-4xl 8xl:text-6xl">
                         {kelvinToCelsiusRounded(this.state.temp)}°
@@ -55,14 +55,14 @@ export class WeatherDisplayComponent extends DisplayComponent<any> {
                 </div>
                 <div className="grid grid-rows-3 sm:gap-0 xl:gap-1">
                     <div className="font-light leading-normal sm:text-xs xl:text-base 2xl:text-xl 4xl:text-2xl 8xl:text-3xl sm:text-left">
-                        precipitation: <span className="font-medium sm:text-sm xl:text-lg 2xl:text-2xl 4xl:text-3xl 8xl:text-5xl sm:text-left">{this.state.precipitation * 100}</span>%
+                        precipitation: <span className="font-medium sm:text-sm xl:text-lg 2xl:text-2xl 4xl:text-3xl 8xl:text-5xl sm:text-left">{this.state.precipitation}</span>%
                     </div>
                     <div className="font-light leading-normal sm:text-xs xl:text-base 2xl:text-xl 4xl:text-2xl 8xl:text-3xl sm:text-left">
                         humidity: <span className="font-medium sm:text-sm xl:text-lg 2xl:text-2xl 4xl:text-3xl 8xl:text-5xl sm:text-left">{this.state.humidity}</span>%
                     </div>
                     <div
                         className="font-light leading-normal sm:text-xs xl:text-base 2xl:text-xl 4xl:text-2xl 8xl:text-3xl sm:text-left">
-                        wind: <span className="font-medium sm:text-sm xl:text-lg 2xl:text-2xl 4xl:text-3xl 8xl:text-5xl sm:text-left">{toKMH(this.state.windSpeed)}</span> km/h
+                        wind: <span className="font-medium sm:text-sm xl:text-lg 2xl:text-2xl 4xl:text-3xl 8xl:text-5xl sm:text-left">{this.state.windSpeed}</span> km/h
                     </div>
                 </div>
         </div>:<div/>);
