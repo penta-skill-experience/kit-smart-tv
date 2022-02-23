d21142b2abd463b6fa9ffc75e285bbdd248295d6
Aaron Siefker <aaron@familie-siefker.de>
2022-02-23 19:41:47 +0100

    commented out not working test

--- src/tests/unit/client/config_website/AdminPage.tests.tsx
+++ src/tests/unit/client/config_website/AdminPage.tests.tsx
@@ -1,25 +1,23 @@
-/*
 import {ConfigWebsite} from "../../../../client/config_website/ConfigWebsite";
 import {AdminPage} from "../../../../client/config_website/AdminPage";
 import React from "react";
 import renderer from 'react-test-renderer';
 
-const handleOldPasswordMock = jest.spyOn(ConfigWebsite.prototype, "handleOldPassword");
-const handleNewPasswordMock = jest.spyOn(ConfigWebsite.prototype, "handleNewPassword");
-const handlePasswordChangeMock = jest.spyOn(ConfigWebsite.prototype, "handlePasswordChange");
+// const handleOldPasswordMock = jest.spyOn(ConfigWebsite.prototype, "handleOldPassword");
+// const handleNewPasswordMock = jest.spyOn(ConfigWebsite.prototype, "handleNewPassword");
+// const handlePasswordChangeMock = jest.spyOn(ConfigWebsite.prototype, "handlePasswordChange");
 
 
-    test("test new password text field for input", () => {
-        const tree = renderer.create(
-            <AdminPage
-                oldPassword={"oldPassword"}
-                newPassword={"newPassword"}
-                handleOldPassword={handleOldPasswordMock}
-                handleNewPassword={handleNewPasswordMock}
-                handlePasswordChange={handlePasswordChangeMock}
-            >
-            </AdminPage>
-        ).toJSON;
-        expect(tree).toMatchSnapshot();
+    xtest("test new password text field for input", () => {
+        // const tree = renderer.create(
+        //     <AdminPage
+        //         oldPassword={"oldPassword"}
+        //         newPassword={"newPassword"}
+        //         handleOldPassword={handleOldPasswordMock}
+        //         handleNewPassword={handleNewPasswordMock}
+        //         handlePasswordChange={handlePasswordChangeMock}
+        //     >
+        //     </AdminPage>
+        // ).toJSON;
+        // expect(tree).toMatchSnapshot();
     });
-*/
