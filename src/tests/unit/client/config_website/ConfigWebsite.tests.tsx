c5cfabbb65f7065dd5098106284151c1c8e0a297
Lukas Xu <utvaz@student.kit.edu>
2022-02-23 16:58:39 +0100

    Added a test to test password input

--- src/tests/unit/client/config_website/ConfigWebsite.tests.tsx
+++ src/tests/unit/client/config_website/ConfigWebsite.tests.tsx
@@ -3,21 +3,30 @@ import {ConfigWebsite} from "../../../../client/config_website/ConfigWebsite";
 import {shallow, configure} from "enzyme";
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import toJson from "enzyme-to-json";
+import {act} from 'react-dom/test-utils';
+
 
 configure({adapter: new Adapter()});
 
-test("snapshot test for config website", () => {
-    /*const tree = renderer.create(<ConfigWebsite></ConfigWebsite>).toJSON;
-    expect(tree).toMatchSnapshot();*/
-    /*const wrapper = shallow(<ConfigWebsite/>);
-    expect(toJson(wrapper)).toMatchSnapshot();*/
-});
-
 describe("hanlde methods test", () => {
+
+    let wrapper;
+    beforeEach(()=>{
+        wrapper =  shallow(<ConfigWebsite/>);
+    });
+
+
    test("snapshot test for login page", async () => {
-       const wrapper = shallow(<ConfigWebsite/>);
-      // console.log(wrapper);
-       await new Promise(process.nextTick);
        expect(toJson(wrapper)).toMatchSnapshot();
    });
+
+    test("test for password input", async () => {
+
+        expect(wrapper.find("LogInPage").at(0).prop("logInInput")).toEqual("");
+        act(() => {
+            wrapper.find("LogInPage").prop("handleInput")({target: {value:"password123"}});
+        })
+        wrapper.update();
+        expect(wrapper.find("LogInPage").at(0).prop("logInInput")).toBe("password123");
+    });
 });
