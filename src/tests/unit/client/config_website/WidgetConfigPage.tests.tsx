683efbb75b11f60f52926d9d15d287d8cdf6caf4
Lukas Xu <utvaz@student.kit.edu>
2022-03-06 15:23:54 +0100

    Added tests for WidgetConfigPage.tests.tsx

--- src/tests/unit/client/config_website/WidgetConfigPage.tests.tsx
+++ src/tests/unit/client/config_website/WidgetConfigPage.tests.tsx
@@ -3,11 +3,14 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import * as React from "react";
 import toJson from "enzyme-to-json";
 import {WidgetConfigPage} from "../../../../client/config_website/WidgetConfigPage";
+import {ConfigComponent} from "../../../../client/widget/ConfigComponent";
 
 
 configure({adapter: new Adapter()});
 
 let wrapper;
+
+//const saveMock = jest.spyOn(ConfigComponent.prototype, "save");
 
 describe("WidgetConfigPage tests", () => {
 
@@ -24,5 +27,27 @@ describe("WidgetConfigPage tests", () => {
     test("snapshot test for WidgetConfigPage", async () => {
         expect(toJson(wrapper)).toMatchSnapshot();
     });
+
+    test("test config button", () => {
+        wrapper.find("ForwardRef(Button)").at(0).simulate("click");
+        expect(wrapper.find("ForwardRef(Dialog)").prop("open")).toEqual(true);
+    });
+
+    test("test close button", () => {
+        wrapper.find("ForwardRef(Dialog)").at(0).simulate("close");
+        expect(wrapper.find("ForwardRef(Dialog)").prop("open")).toEqual(false);
+    });
+
+    test("test config cancel button", () => {
+        wrapper.find("ForwardRef(Button)").at(1).simulate("click");
+        expect(wrapper.find("ForwardRef(Dialog)").prop("open")).toEqual(false);
+    });
+
+    /*test("test config ok button", () => {
+        saveMock.mockImplementation();
+        wrapper.find("ForwardRef(Button)").at(2).simulate("click");
+        expect(wrapper.find("ForwardRef(Dialog)").prop("open")).toEqual(false);
+    });*/
+
 
 });
