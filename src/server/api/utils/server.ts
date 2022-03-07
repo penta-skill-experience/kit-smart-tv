0da624fb1412110d5d112db346d696950a2cd4d0
tom-mohr <tom-mohr@users.noreply.github.com>
2022-03-07 15:07:23 +0100

    small reformatting

--- src/server/api/utils/server.ts
+++ src/server/api/utils/server.ts
@@ -1,17 +1,14 @@
-import express from "express";
+import express, {Express} from "express";
 import routes from "../routes";
 import deserializeAdmin from "../middleware/deserializeAdmin";
 
-function createServer() {
+export default function createServer(): Express {
     const app = express();
 
     app.use(express.json());
-
     app.use(deserializeAdmin);
 
     routes(app);
 
     return app;
 }
-
-export default createServer;
