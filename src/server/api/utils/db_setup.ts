45a020eb41f4113904f9eab29ff727d57e0ea9ff
tom-mohr <tom-mohr@users.noreply.github.com>
2022-03-21 10:26:01 +0100

    default DB values for announcements

--- src/server/api/utils/db_setup.ts
+++ src/server/api/utils/db_setup.ts
@@ -2,11 +2,13 @@ import config from "./default_values/config.json";
 import users from "./default_values/users.json";
 import values from "./default_values/values.json";
 import widgets from "./default_values/widgets.json";
+import announcements from "./default_values/announcements.json";
 import {createValues} from "../services/values.services";
 import {createConfig} from "../services/config.services";
 import {createUsers} from "../services/users.services";
 import {createWidgetData} from "../services/widgetData.service";
 import {createAdmin, getAdmin} from "../services/admin.service";
+import {createAnnouncements} from "../services/announcements.services";
 
 export async function ensureDatabaseContent(): Promise<void> {
     return getAdmin()
@@ -22,4 +24,5 @@ async function initDb() {
     await createUsers(users);
     await createValues(values);
     await createWidgetData(widgets);
+    await createAnnouncements(announcements);
 }
